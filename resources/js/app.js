22552ec6ae125bc0121f197a45c34d68f43caa21
Fil <filjoseph22@gmail.com>
2020-03-01 07:48:15 +0800

    Created a simple login form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+const Vue = require('vue');
+
+Vue.component('my-component-name', {});
+
+const vm = new Vue({
+    el: '#app'
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,67 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Referral System</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+
+    <body class="bg-teal-700">
+        <div id="app" class="">
+            {{--
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -77,24 +26,32 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="login_form">
+                <div class="login_form_wrapper m-10">
+                    <h1 class="my-2 text-3xl text-white">Login Form</h1>
+    
+                    <div class="bg-white border p-5 rounded shadow-2xl">
+                        <form action="" method="post" class="form">
+                            @csrf
+                            
+                            <label for="email">Email</label>
+                            <input type="email" class="border mb-3 p-1 pl-2" placeholder="Enter your email" required>
+    
+                            <label for="password">Password</label>
+                            <input type="password" class="border mb-3 p-1 pl-2" placeholder="Enter your password" required>
+    
+                            <div class="my-2">
+                                <button type="submit" class="bg-blue-500 border p-1 p-2 pl-5 pr-5 rounded text-white uppercase">Login</button> 
+                                <button type="submit" class="bg-orange-400 border p-1 p-2 pl-5 pr-5 rounded text-white uppercase">Sign Up</button> 
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+
+    <script src="{{ mix('js/app.js') }}?v=0.1"></script>
     </body>
 </html>
