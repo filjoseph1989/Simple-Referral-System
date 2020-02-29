9cf8c692462dadce76604034a151464955c0f7ed
Fil <filjoseph22@gmail.com>
2020-03-01 05:48:25 +0800

    Setup tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,15 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.js('resources/js/app.js', 'public/js');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.postCss('resources/sass/app.css', 'public/css', [
+    require('postcss-import'),
+    require('autoprefixer'),
+    require('postcss-nested'),
+    require('tailwindcss'),
+]);
+
+if (mix.inProduction()) {
+    mix.sourceMaps().version();
+}
