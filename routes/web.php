40da9a548af758150b8d36f155eb2d684768d6ab
Fil <filjoseph22@gmail.com>
2020-03-02 01:05:11 +0800

    Preapre web routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index')->name('get.home');
+Route::post('/login', 'Auth\LoginController@login')->name('post.login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('post.logout');
+Route::get('/signup', 'Auth\RegisterController@showRegistrationForm')->name('get.signup.form');
+Route::post('/signup', 'Auth\RegisterController@register')->name('post.signup');
+Route::get('/invite/{code}', 'ReferralController@index')->name('get.invite');
