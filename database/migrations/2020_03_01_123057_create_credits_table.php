ac5c0126e420cdcd354466c86bf9e0568a5a30b9
Fil <filjoseph22@gmail.com>
2020-03-02 00:55:06 +0800

    Added migrations

---
+++ database/migrations/2020_03_01_123057_create_credits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credits', function (Blueprint $table) {
+            $table->bigIncrements('id');           
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');            
+            $table->integer('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credits');
+    }
+}
