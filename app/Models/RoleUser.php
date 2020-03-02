c91f630e4eef1c1257f029457b7b5e8f56098131
Fil <filjoseph22@gmail.com>
2020-03-03 04:56:10 +0800

    Added relation to role

--- app/Models/RoleUser.php
+++ app/Models/RoleUser.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoleUser extends Model
 {
-    //
+    /**
+     * Role is part of table roles
+     *
+     * @return object
+     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
