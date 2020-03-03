d757c872bff7560a52121f0e8db268d60a41f975
Fil <filjoseph22@gmail.com>
2020-03-04 02:00:18 +0800

    used message components

--- resources/views/giftcard/index.blade.php
+++ resources/views/giftcard/index.blade.php
@@ -1,19 +1,5 @@
 @section('content')
-    @if ($errors->any())
-        <div class="border mb-2 p-2 rounded bg-white">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li class="text-red-600 text-sm">{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    @if (!is_null(session('status')))
-        <div class="bg-white border border-green-500 mb-2 p-2 rounded text-sm">
-            <p class="{{ session('status')['class'] ?? '' }}">{{ session('status')['message'] ?? '' }}</p>
-        </div>
-    @endif
+    @include('Compoments.message')
 
     @include('Compoments.create_gift_cards')
 
