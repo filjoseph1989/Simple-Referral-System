e41df57963a1294896c6ab4fc822a88751e8e7e8
Fil <filjoseph22@gmail.com>
2020-03-04 00:27:43 +0800

    Welcome user with gift cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @section('content')
+    <h1 class="font-bold mt-10 text-5xl text-center text-white">Gift Cards</h1>
+
+    <div class="container flex flex-wrap mt-10 my-auto" style="justify-self: center;">
+        @foreach ($giftCard as $key => $value)
+            @include('Compoments.gift_card')
+        @endforeach
+    </div>
 @endsection
 
 @section('css')
