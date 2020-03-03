ee203446faeb8bfc27fb5503299a456e4f99a675
Fil <filjoseph22@gmail.com>
2020-03-04 00:12:56 +0800

    Add gift card relation

--- app/Models/GiftCardUser.php
+++ app/Models/GiftCardUser.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class GiftCardUser extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'gift_card_id', 'quantity'
+    ];
+
+    /**
+     * Owned by gif card
+     *
+     * @return object
+     */
+    public function gift_card()
+    {
+        return $this->belongsTo(GiftCard::class);
+    }
 }
