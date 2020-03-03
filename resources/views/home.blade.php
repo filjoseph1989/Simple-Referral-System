f8ce108a7b32768f2c64f1deee22216affa2b7c5
Fil <filjoseph22@gmail.com>
2020-03-04 00:28:03 +0800

    Moved message as components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,5 @@
 @section('content')
-    @if ($errors->any())
-        <div class="border mb-2 p-2 rounded bg-white">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li class="text-red-600 text-sm">{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    @if (!is_null(session('status')))
-        <div class="bg-white border border-green-500 mb-2 p-2 rounded text-sm">
-            <p class="{{ session('status')['class'] ?? '' }}">{{ session('status')['message'] ?? '' }}</p>
-        </div>
-    @endif
+    @include('Compoments.message')
 
     @include('Compoments.welcome')
 
@@ -21,7 +7,7 @@
 
     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
         <div class="flex items-center justify-center w-full" style=" height: 150px;">
-            <div class="border flex items-center justify-center p-5 text-lg font-bold points">
+            <div class="flex items-center justify-center p-5 text-lg font-bold points">
                 {{ $credit->points ?? '0' }} point{{ (isset($credit->points) && $credit->points > 0) ? "s" : '' }}
             </div>
         </div>
